bc165499b4fd7b689ee46b0640255b28ef0e0c8c
0xD185 <98742153+0xD185@users.noreply.github.com>
2022-12-12 14:34:25 +0000

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY,process.env.USER1_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+};
