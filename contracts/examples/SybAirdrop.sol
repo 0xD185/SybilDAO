389171e46d2ae7a4b8a301b2a408ea0709bf53a1
0xD185 <98742153+0xD185@users.noreply.github.com>
2022-12-13 07:08:35 +0000

    Update SybAirdrop.sol

--- contracts/examples/SybAirdrop.sol
+++ contracts/examples/SybAirdrop.sol
@@ -6,26 +6,33 @@ pragma solidity ^0.8.17;
 */
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 interface ISybil is IERC20 {
     function check(address) external view returns (bool);
 }
 
-contract SybAirdrop {
+contract SybAirdrop is Ownable {
     mapping (address => bool) public claimed;
 
     address constant sybil = 0x7927BEa1eA84614DCeAECa1710cea8a7DeAa1d25;
 
-    function airdrop() public {
+    function airdrop() external {
         require(ISybil(sybil).check(msg.sender),"Visit: https://sybildao.com/#verify");
         require(claimed[msg.sender] == false, "Already claimed");
+        claimed[msg.sender] = true;
         ISybil(sybil).transfer(msg.sender, 100 * 1e18);
     }
 
-    function checkEligible() public view returns(bool) {
+    function checkEligible(address _user) external view returns(bool) {
         bool eligible = true;
-        if (ISybil(sybil).check(msg.sender) == false) eligible = false;
-        if (claimed[msg.sender] == true) eligible = false;
+        if (ISybil(sybil).check(_user) == false) eligible = false;
+        if (claimed[_user] == true) eligible = false;
         return eligible;
+    }
+
+    function reclaim() external onlyOwner {
+        uint256 balance = IERC20(sybil).balanceOf(address(this));
+        IERC20(sybil).transfer(msg.sender, balance);
     }
 }
